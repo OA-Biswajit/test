50a1ae917223db4892514fce69203035187ffb46
OA-Biswajit <biswajitkumarsenapati2003@gmail.com>
2023-03-29 20:47:32 +0530

    added comments

--- pratice/p1.java
+++ pratice/p1.java
@@ -1,14 +1,17 @@
 class p1{
     public static void main(String[] args) {
         int n=5;
+        // for rows
         for (int i = 1; i < n; i++) {
+            // for spaces
             for (int j =1 ; j <=n-i; j++) {
                 System.out.print(" ");
             }
+            // first half
             for (int j = i; j >=1 ; j--) {
                 System.out.print(j);
             }
-          
+        //   second half
             for (int j = 2; j <= i; j++) {
                 System.out.print(j);
             }
